6ffbd3797ae97ef53e25f4b1e869e93621bf7724
Hank McCord <hymccord@gmail.com>
2023-09-11 15:50:03 -0400

    Use declarativeNetRequest to block embeds

--- Extension/content.js
+++ Extension/content.js
@@ -1,26 +1,5 @@
-console.log('Content removal script has started.')
-// Function to remove the entire div containing Twitch embeds
-function removeTwitchEmbeds () {
-  // Find all script tags on the page
-  const scripts = document.querySelectorAll('script')
-
-  // Loop through all script tags
-  for (const script of scripts) {
-    // Check if the script's 'src' attribute contains the Twitch embed URL
-    if (
-      script.src &&
-      script.src.includes('https://player.twitch.tv/js/embed/v1.js')
-    ) {
-      console.log('Found a Twitch embed script.')
-      const parentDiv = script.closest('.fixme.hidden-sm')
-
-      // Remove the parent div from the DOM if it exists
-      if (parentDiv) {
-        console.log('Removing embeded script')
-        parentDiv.remove()
-      }
-    }
-  }
+// Function to clean html after blocking embeds
+function cleanupHTML () {
 
   // Find all h2 tags on the page
   const h2s = document.querySelectorAll('h2')
@@ -42,5 +21,5 @@ function removeTwitchEmbeds () {
   }
 }
 
-// Run the function to remove the entire div containing Twitch embeds
-removeTwitchEmbeds()
+// Tidy up the page
+cleanupHTML()
