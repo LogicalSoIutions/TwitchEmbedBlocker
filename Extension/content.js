f517841c63ee9d795918b9962e071d2e27467196
Zach Clinger <staff@illogical.network>
2023-10-25 11:22:19 -0700

    clean up + add new website

--- Extension/content.js
+++ Extension/content.js
@@ -1,35 +1,67 @@
-// Function to clean html after blocking embeds
+let embedStatus = true;
+
 function cleanupHTML () {
 
-  // Find all h2 tags on the page
+  // eft-ammo.com + fextralife
   const h2s = document.querySelectorAll('h2')
 
-  // Loop through all h2 tags
   for (const h2 of h2s) {
-    // Check if the h2's text includes the Twitch stream text
     if (
       h2.textContent &&
       h2.textContent.includes('Watch') &&
       h2.textContent.includes('stream here')
     ) {
-      // Find the closest parent div and remove it from the DOM
       const parentDiv = h2.closest('div')
       if (parentDiv) {
         parentDiv.remove()
+        embedStatus = false;
       }
     }
   }
 
+  // albiononline2d.com 
   const iframes = document.querySelectorAll('iframe');
   for (const iframe of iframes) {
-    if (iframe.src && iframe.src.includes('https://player.twitch.tv/') && iframe.src.includes('parent=albiononline2d.com')) {
+    if (iframe.src && iframe.src.includes('https://player.twitch.tv/')) {
       const parentDiv = iframe.closest('div.col-12.text-center');
       if (parentDiv) {
         parentDiv.remove();
+        embedStatus = false;
       }
     }
   }
-  
+
+  // nwdb.info
+  const twitchEmbed = document.getElementById('twitch-embed-floating');
+  if (twitchEmbed) {
+    console.log("detected floating embed")
+    twitchEmbed.remove();
+    embedStatus = false;
+  }
+
 }
 
-cleanupHTML()
+// cleanup html in observer because of nwdb.info loading style
+
+function observeDOM() {
+
+  const target = document.body;
+  const observer = new MutationObserver((mutations, observerInstance) => {
+
+    if (!embedStatus) {
+      console.log("Embed(s) removed, disconnecting the observer");
+      observerInstance.disconnect(); 
+      return; // Exit the function early since there's nothing left to do
+    }
+
+    cleanupHTML();
+
+
+  });
+
+  const config = { childList: true, subtree: true };
+
+  observer.observe(target, config);
+}
+
+observeDOM();
